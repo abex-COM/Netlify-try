9d19209b48d0415ba7bee0884bf7c7c690d84b7d
Abdulaki <abdulakimustefa@gmail.com>
2024-08-11 14:25:58 +0300

    add some

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,8 @@
 export default function App() {
-  return <div className="h-96 bg-sky-600 text-sky-50">Deployed on netlfiy</div>;
+  return (
+    <div className="h-96 bg-sky-600 text-sky-50 flex justify-center">
+      Deployed on netlfiy
+      <button className="border w-20 h-10 bg-yellow-600 text-y">Clicke </button>
+    </div>
+  );
 }
